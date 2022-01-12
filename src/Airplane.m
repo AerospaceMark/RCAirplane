232de832de47a5828e2eb53f7f5f1b48e8747fac
Mark C. Anderson <anderson.mark.az@gmail.com>
2022-01-11 20:54:51 -0700

    Initial work complete

--- src/Airplane.m
+++ src/Airplane.m
@@ -7,29 +7,36 @@ classdef Airplane
         CL_Max = []; % The maximum lift coefficient
         Speed = []; % The current speed
         CL = []; % The current lift coefficient
+        CD = []; % The current drag coefficient
         CD_Zero_Lift = []; % The no-lift drag coefficient
         CD_Parasitic = []; % The parasitic drag coefficient
         
         % Wing Properties
-        Wing_Area = [];
-        Mean_Aerodynamic_Chord = [];
-        Wing_Span = [];
+        Wing_Area = []; % ft^2
+        Mean_Aerodynamic_Chord = []; % ft
+        Wing_Span = []; % ft
         Inviscid_Efficiency = [];
         
         % Horizontal Stabilizer Properties
-        Horizontal_Tail_Area = [];
-        Horizontal_Tail_Lever_Arm = [];
+        Horizontal_Tail_Area = []; % ft^2
+        Horizontal_Tail_Lever_Arm = []; % ft
         
         % Vertical Stabilizer Properties
-        Vertical_Tail_Area = [];
-        Vertical_Tail_Lever_Arm = [];
+        Vertical_Tail_Area = []; % ft^2
+        Vertical_Tail_Lever_Arm = []; % ft
         
         % Propulsion Properties
-        Propulsion_Power = [];
-        Total_Thrust = [];
+        Propulsion_Power = []; % Watts
+        Total_Thrust = []; % pounds
+        Battery_Weight = []; % pounds
+        Battery_Total_Energy = []; % Watt-hours
+        Propulsion_Efficiency = [];
+        
+        % Other Properties
+        Bank_Angle = []; % degrees
         
         % Ambient Properties
-        Air_Density = [];
+        Air_Density = []; % slugs/ft^3
         Gravitational_Acceleration = 32.2; % ft/s^2
         Kinematic_Viscosity = 1.5723e-4; % ft^2/s
         
@@ -39,15 +46,23 @@ classdef Airplane
         
         Horizontal_Tail_Volume
         Vertical_Tail_Volume
-        Wing_Loading
-        Stall_Speed
-        Lift
+        Wing_Loading % lbs / ft^2
+        Stall_Speed % mph
+        Lift % pounds
+        Drag % pounds
         Dynamic_Pressure
-        Takeoff_Distance
-        Maximum_Speed
+        Takeoff_Distance % ft
+        Maximum_Speed % mph
         CL_For_Max_Efficiency
+        CL_For_Max_Endurance
         Reynolds_Number
         Aspect_Ratio
+        Range % miles
+        Endurance % hours
+        Lift_To_Drag_Ratio
+        Rate_Of_Climb % ft/s
+        Turning_Radius % ft
+        Load_Factor
         
     end
         
@@ -145,6 +160,66 @@ classdef Airplane
             
             % FIXME: Use generalized reference area instead of wing area
             value = sqrt(obj.CD_Parasitic .* pi .* obj.Aspect_Ratio .* obj.Inviscid_Efficiency);
+            
+        end
+        
+        function value = get.Drag(obj)
+            
+            % FIXME: Use generalized reference area instead of wing area
+            value = obj.CD .* obj.Dynamic_Pressure .* obj.Wing_Area;
+            
+        end
+        
+        function value = get.Lift_To_Drag_Ratio(obj)
+            
+            value = obj.CL ./ obj.CD;
+            
+        end
+        
+        function value = get.Range(obj)
+            
+            % Convert energy density to metric and seconds
+            value = 3600 .* obj.Battery_Total_Energy ./ (obj.Battery_Weight .* 4.448);
+            value = value ./ (obj.Gravitational_Acceleration .* 0.3048);
+            value = value .* obj.Propulsion_Efficiency;
+            value = value .* obj.Lift_To_Drag_Ratio;
+            value = value .* obj.Battery_Weight ./ obj.Weight;
+            
+            % convert to miles
+            value = value ./ 1609;
+            
+        end
+        
+        function value = get.Endurance(obj)
+            
+            value = obj.Range ./ obj.Speed;
+            
+        end
+        
+        function value = get.CL_For_Max_Endurance(obj)
+            
+            value = sqrt(3 .* obj.CD_Parasitic .* pi .* obj.Aspect_Ratio .* obj.Inviscid_Efficiency);
+            
+        end
+        
+        function value = get.Rate_Of_Climb(obj)
+            
+            value = obj.Speed .* (obj.Total_Thrust - obj.Drag) ./ obj.Weight;
+            
+            % convert mph to ft/s
+            value = value * 1.467;
+            
+        end
+        
+        function value = get.Turning_Radius(obj)
+            
+            value = obj.Speed.^2 / (obj.Gravitational_Acceleration .* tand(obj.Bank_Angle));
+            
+        end
+        
+        function value = get.Load_Factor(obj)
+            
+            value = 1 ./ cosd(obj.Bank_Angle);
             
         end
         
--- test/Overall_Test.m
+++ test/Overall_Test.m
@@ -5,7 +5,8 @@ RC = Airplane;
 % Overall Properties
 RC.Weight = 15; % pounds
 RC.CL_Max = 1.278;
-RC.CL = 0.55; % the current CL (not max)
+RC.CL = 0.227; % the current CL (not max)
+RC.CD = 0.014; % the current CD
 RC.Speed = 29.88; % current speed (mph)
 RC.CD_Zero_Lift = 0.01;
 RC.CD_Parasitic = 0.003;
@@ -26,7 +27,13 @@ RC.Vertical_Tail_Lever_Arm = 4.8; % ft
 
 % Propulsion Properties
 RC.Propulsion_Power = 680; % Watts
-RC.Total_Thrust = 20; % Pounds
+RC.Total_Thrust = 20; % pounds
+RC.Battery_Weight = 2; % pounds
+RC.Battery_Total_Energy = 1000; % Watt-hours
+RC.Propulsion_Efficiency = 0.5;
+
+% Other
+RC.Bank_Angle = 10; % Degrees
 
 % Ambient Properties
 RC.Air_Density = 0.002377; % slugs / ft^3
