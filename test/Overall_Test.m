2e4be1f1b0cbec79469fa9358aab24357fceceb4
Mark C. Anderson <anderson.mark.az@gmail.com>
2022-01-14 17:41:25 -0700

    Designed DBF 2022

--- test/Overall_Test.m
+++ test/Overall_Test.m
@@ -5,31 +5,31 @@ RC = Airplane;
 % Overall Properties
 RC.Weight = 15; % pounds
 RC.CL_Max = 1.278;
-RC.CL = 0.227; % the current CL (not max)
-RC.CD = 0.014; % the current CD
-RC.Speed = 29.88; % current speed (mph)
+RC.CL = 0.41; % the current CL (not max)
+RC.CD = 0.005; % the current CD
+RC.Speed = 50; % current speed (mph)
 RC.CD_Zero_Lift = 0.01;
-RC.CD_Parasitic = 0.003;
+RC.CD_Parasitic = 0.01;
 
 % Wing Properties
 RC.Wing_Span = 8; % feet
-RC.Aspect_Ratio = 3:0.1:10;
+RC.Aspect_Ratio = 5;
 RC.Inviscid_Efficiency = 1.0;
 
 % Horizontal Stabilizer Properties
-RC.Horizontal_Tail_Area = 2.1; % ft^2
+RC.Horizontal_Tail_Area = 3.0; % ft^2
 RC.Horizontal_Tail_Lever_Arm = 4.8; % ft
 
 % Vertical Stabilizer Properties
-RC.Vertical_Tail_Area = 1.05; % ft^2
+RC.Vertical_Tail_Area = 1.0; % ft^2
 RC.Vertical_Tail_Lever_Arm = 4.8; % ft
 
 % Propulsion Properties
-RC.Propulsion_Power = 680; % Watts
-RC.Total_Thrust = 20; % pounds
-RC.Battery_Weight = 2; % pounds
-RC.Battery_Total_Energy = 1000; % Watt-hours
-RC.Propulsion_Efficiency = 0.5;
+RC.Propulsion_Power = 668; % Watts
+RC.Total_Thrust = 11; % pounds
+RC.Battery_Weight = 1.3; % pounds
+RC.Battery_Total_Energy = 100; % Watt-hours
+RC.Propulsion_Efficiency = 0.3;
 
 % Other
 RC.Bank_Angle = 10; % Degrees
